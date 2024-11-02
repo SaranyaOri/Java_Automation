ab6e5ea6b916ab10b5f14e627f41d01fe7286a2c
SARANYA <saranya.cs07@gmail.com>
2024-11-02 12:25:05 +0530

    Commit

--- src/Task/User1.java
+++ src/Task/User1.java
@@ -1,7 +1,10 @@
 package Task;
 
+import java.util.Scanner;
+
 public class User1 {
     public static void main(String[] args) {
         System.out.println();
+        Scanner scanner = new Scanner(System.in);
     }
 }
